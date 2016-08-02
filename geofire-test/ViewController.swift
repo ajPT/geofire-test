9c5c0c504c92fcb651c13c20d6f32c8128843068
Amadeu Andrade <amadeuandrade@amadeus-mbp.lan>
2016-08-02 21:15:19 +0100

    Initial Commit

---
+++ geofire-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  geofire-test
+//
+//  Created by Amadeu Andrade on 02/08/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
